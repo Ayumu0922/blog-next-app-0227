ac4614a3ef7919c7d58210410ee529762ebe5b1f
Ayumu0922 <9bota.ayu6@yahoo.ne.jp>
2024-02-27 17:23:57 +0900

    create blog component

---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,26 @@
+import { posts } from "@/data/posts";
+import Link from "next/link";
+import React from "react";
+
+const BlogsPage = () => {
+  return (
+    <div className=" max-w-4xl mx-auto py-8">
+      <h1 className=" text-3xl font-bold mb-4 ">
+        <div className=" grid grid-cols-1  sm:grid-cols-2 md:grid-cols-3 gap-6">
+          {posts.map((post) => (
+            <Link
+              href={`/blogs/${post.id}`}
+              key={post.id}
+              className="glassBackgroundColor rounded-sm p-3"
+            >
+              <h2 className=" text-xl font-bold">{post.title}</h2>
+              <p>Written by: {post.username}</p>
+            </Link>
+          ))}
+        </div>
+      </h1>
+    </div>
+  );
+};
+
+export default BlogsPage;
---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className=" bg-white flex justify-center  items-center  h-screen font-bold text-3xl">
+      NotFound
+    </div>
+  );
+};
+
+export default NotFound;
